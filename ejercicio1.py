46d0ec87f04c7d23d0c0cde96897a068c1bd64c1
maria2332 <maria.ballesteros2004@gmail.com>
2022-12-13 21:48:35 +0100

    cambio

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+"""
+Ejercicio 1
+
+El trabajo que realizaremos consiste en la elaboración de un modelo para resolver sudokus, para ello, crea una función que resuelva un Sudoku de 9x9.
+Un sudoku es un puzzle matemático que se construye sobre una rejilla de 9x9 casillas. El objetivo es rellenarla con números enteros del 1 al 9 según las siguientes restricciones:
+No se pueden repetir números en una misma fila.
+No se pueden repetir números en una misma columna.
+No se pueden repetir números en una misma subcuadrícula de 3x3 de las 9 que componen la rejilla.
+Esta función recibe un argumento que consiste en una matriz de 2D. Un valor de 0 representa un cuadrado desconocido.
+El sudoku a resolver ha de estar bien planteado, i.e., ha de tener solución única. Esto se cumple si se parte de unas condiciones iniciales adecuadas, que consisten en números ya fijados en algunas de las casillas del sudoku. Un sudoku bien planteado sin resolver podría ser, e.g., el mostrado en la siguiente figura:
+
+"""
+
+
+
---
+++ ejercicio5.py
@@ -0,0 +1,34 @@
+"""
+El cifrado Rail Fence (también llamado cifrado en zigzag) es una forma de cifrado por transposición. Funciona escribiendo su mensaje en líneas alternas a lo largo de la página y luego leyendo cada línea por turno. Veamos el desglose paso a paso para cifrar un texto usando Rail Fence Cipher:
+
+Para encriptar un mensaje usando Rail Fence Cipher, primero necesitamos tener una clave (lo mismo para encriptar y desencriptar), que es el número de filas que tendrá para este cifrado.
+Luego comenzamos a escribir las letras del texto sin formato dado en diagonal hacia el lado derecho hasta alcanzar el número de filas especificado por la clave.
+Luego rebotamos hacia arriba de manera similar en diagonal hasta que llegamos a la primera fila nuevamente. Así, los alfabetos del texto plano se escriben en zig-zag.
+Este ciclo continúa hasta que se alcanza el final del texto sin formato. Luego se leen las filas individuales para obtener el texto cifrado.
+Considerar un ejemplo nos aclararía las cosas. Tomemos un ejemplo donde: El texto sin formato se da como " defend the east wall " y el número de rieles (clave) = 3, luego el proceso de encriptación es como se muestra a continuación:
+El cifrado Rail Fence con una clave de 3
+
+Tenga en cuenta que al final del mensaje hemos insertado dos letras "X", que se llaman nulos y actúan como marcadores de posición. Esto se hace para asegurarse de que el mensaje encaje perfectamente en la cuadrícula, de modo que haya el mismo número de letras en la fila superior y en la fila inferior.
+
+Ahora, leer la imagen fila por fila nos da el texto cifrado como "DNETLEEDHESWLXFTAAX".
+
+Para descifrar un texto cifrado codificado con Rail Fence Cipher, tenemos que reconstruir la cuadrícula diagonal utilizada para cifrar el mensaje. Comenzamos escribiendo el mensaje, pero dejando una estrella en lugar de los espacios por ocupar (como se muestra en la siguiente figura). Gradualmente, puede reemplazar todas las estrellas con las letras correspondientes y leer el texto sin formato de la tabla de manera similar al cifrado
+
+Colocación de estrellas en el lugar de los espacios a ocupar
+
+Veamos el desglose por pasos:
+
+Comenzamos haciendo una cuadrícula (matriz de rieles) con tantas filas como la clave dada (número de rieles) y tantas columnas como la longitud del texto cifrado.
+Luego colocamos la primera letra en el cuadrado superior izquierdo y avanzamos en diagonal hacia abajo donde están presentes las letras.
+Cuando volvemos a la fila superior, colocamos la siguiente letra en el texto cifrado. Continuamos este proceso a lo largo de la fila y comenzamos la siguiente fila cuando lleguemos al final.
+Atravesamos la matriz en forma de zig-zag para obtener el texto plano original.
+ Escriba una función/método que tome 2 argumentos, una cadena y el número de rieles, y devuelva la cadena CODIFICADA.
+
+Escriba una segunda función/método que tome 2 argumentos, una cadena codificada y el número de rieles, y devuelva la cadena DECODIFICADA.
+
+Tanto para la codificación como para la decodificación, asuma un número de rieles >= 2 y que pasar una cadena vacía devolverá una cadena vacía.
+
+Tenga en cuenta que el ejemplo anterior excluye la puntuación y los espacios solo por simplicidad. Sin embargo, hay pruebas que incluyen puntuación. No filtre la puntuación, ya que son parte de la cadena.
+"""
+
+
